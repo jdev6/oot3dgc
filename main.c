c579c56eae79439e69c56741c07822272531f780
jdev6 <jdev6@protonmail.com>
2018-07-12 17:53:03 +0200

    piss

--- main.c
+++ main.c
@@ -14,15 +14,14 @@ void click (Display *display, int button) {
   event.xbutton.button = button;
   event.xbutton.same_screen = True;
   event.xbutton.subwindow = DefaultRootWindow (display);
-  while (event.xbutton.subwindow)
-    {
+  while (event.xbutton.subwindow) {
       event.xbutton.window = event.xbutton.subwindow;
       XQueryPointer (display, event.xbutton.window,
 		     &event.xbutton.root, &event.xbutton.subwindow,
 		     &event.xbutton.x_root, &event.xbutton.y_root,
 		     &event.xbutton.x, &event.xbutton.y,
 		     &event.xbutton.state);
-    }
+  }
   // Press
   event.type = ButtonPress;
   if (XSendEvent (display, PointerWindow, True, ButtonPressMask, &event) == 0)
